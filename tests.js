7ef927050504171283c8ccec913109b290d469b2
Jonida Sinaj <jonida97sinaj@gmail.com>
2018-01-13 21:49:01 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 var index = require("./index.js")
 
-if (index.name === "Susan") {
+if (index.name == "Susan") {
     console.log("The name is correct")
 } 
 else {
@@ -8,7 +8,7 @@ else {
 }
 
 
-if (index.height === 70) {
+if (index.height == 70) {
     console.log("The height is correct")
 } 
 else {
